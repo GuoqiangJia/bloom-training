31c04740563e1d95f3bbc7fedb221fe15eca059c
Guoqiang <guoqiang_thinkpad@genhot.cn>
2023-05-17 16:20:57 +0800

    - Training script

--- train/peft_torchrun.sh
+++ train/peft_torchrun.sh
@@ -1,20 +1,20 @@
-#!/bin/bash
-
 model_name_or_path=bigscience/bloomz-7b1-mt
-data_path=data/data.json
 model_max_length=2048
+data_path=data/data.json
 output_dir=checkpoints/llms_bloom_7b/
 
-accelerate launch --config_file ./peft/accelerate_config.yaml ./peft/train.py \
+torchrun \
+  --nnodes=1 \
+  train.py \
   --model_name_or_path ${model_name_or_path} \
   --model_max_length ${model_max_length} \
   --data_path ${data_path} \
   --output_dir ${output_dir} \
   --bf16 True \
   --num_train_epochs 3 \
-  --per_device_train_batch_size 1 \
-  --per_device_eval_batch_size 1 \
-  --gradient_accumulation_steps 1 \
+  --per_device_train_batch_size 4 \
+  --per_device_eval_batch_size 4 \
+  --gradient_accumulation_steps 8 \
   --save_strategy "steps" \
   --save_steps 500 \
   --evaluation_strategy "no" \
@@ -24,9 +24,7 @@ accelerate launch --config_file ./peft/accelerate_config.yaml ./peft/train.py \
   --warmup_ratio 0.03 \
   --lr_scheduler_type "cosine" \
   --logging_steps 1 \
+  --fsdp "full_shard auto_wrap" \
+  --fsdp_transformer_layer_cls_to_wrap 'BloomBlock' \
   --tf32 True \
-  --gradient_checkpointing True \
-  --lora True \
-  --lora_r 16 \
-  --lora_alpha 16 \
-  --lora_dropout 0.05
+  --gradient_checkpointing True
