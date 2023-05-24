e20e599fceb42e1ec3ee6d3fe96c8fb6fbc26045
Guoqiang <guoqiang_thinkpad@genhot.cn>
2023-05-24 15:15:14 +0800

    - Training script

--- train/peft_accelerate.sh
+++ train/peft_accelerate.sh
@@ -14,7 +14,6 @@ accelerate launch --config_file ./peft/accelerate_config.yaml ./peft/train.py \
   --num_train_epochs 3 \
   --per_device_train_batch_size 1 \
   --per_device_eval_batch_size 1 \
-  --gradient_accumulation_steps 1 \
   --save_strategy "steps" \
   --save_steps 500 \
   --evaluation_strategy "no" \
